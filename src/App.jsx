2f0b13e5df487b8b1cf7365f3a9f3e5264878787
Frankin Angarita <128552142+Frankin16@users.noreply.github.com>
2023-07-05 19:00:36 -0500

    api-clima

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const ApiEndpoint = "https://api.openweathermap.org/data/2.5/weather?"
+const ApiKey = "bb12cf61ee6723c202343dfefde65708"
+const Units = "&units=metric"
+
+
+
+
+function App() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [respData, setRespData] = useState({});
+  const [isMeter, setIsMeter] = useState(true);
+  // const temperature = {respData.main?.temp}
+
+  const changeUnits = () => {
+    setIsMeter(!isMeter)
+  }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    })
+    let finalApiEndpoint = `${ApiEndpoint}lat=${latitude}&lon=${longitude}&appid=${ApiKey}${Units}`;
+    axios
+    
+      .get(finalApiEndpoint)
+      .then(resp => {
+        setRespData(resp.data)
+      })
+
+     
+  })
+ 
+  
+ 
+
+
+  return (
+    <>
+    <section>
+      <h1>Weather App</h1>
+    </section>
+      <div className='container'>
+        <div className='column1'>
+          <h2 className='temp'>{Math.round(isMeter ? respData.main?.temp : respData.main?.temp * 1.8 + 32)}°</h2>
+          <h3>Wind:{respData.wind?.speed}</h3>
+          <h3>Pressure:{respData.main?.pressure}</h3>
+          <h3>Humidity:{respData.main?.humidity}</h3>
+        <h2>{respData.name}, {respData.sys?.country}</h2>
+        </div>
+
+        <div className='column2'>
+            {respData.weather ? <img src={`https://openweathermap.org/img/wn/${respData.weather[0].icon}@4x.png`} alt="weather icon"/> : null } 
+            {respData.weather ? <h3>{respData.weather[0].description}</h3> : null }
+            
+        </div>
+        
+      </div>
+      <section className='button'>
+        <button onClick={changeUnits} className='change'>Change to Fahrenheit</button>
+      </section>
+    </>
+  )
+}
+
+export default App
